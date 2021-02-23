b0bd2a63a19391f48f517346e085d1721d02faf7
worthyd-ru <57643926+worthyd-ru@users.noreply.github.com>
2021-02-23 15:04:51 +0000

    Deploying to gh-pages from master @ fdf920477ece62e5286bad7fff374edf02667cce 🚀

--- bicycle.tsx
+++ bicycle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="bicycle" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="bicycle.png" width="512" height="512"/>
+<tileset version="1.4" tiledversion="1.4.3" name="bicycle" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="images/bicycle.png" width="512" height="512"/>
 </tileset>
--- fence.tsx
+++ fence.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="fence" tilewidth="32" tileheight="32" tilecount="90" columns="10">
- <image source="fence.png" width="320" height="288"/>
+<tileset version="1.4" tiledversion="1.4.3" name="fence" tilewidth="32" tileheight="32" tilecount="90" columns="10">
+ <image source="images/fence.png" width="320" height="288"/>
 </tileset>
