fdf920477ece62e5286bad7fff374edf02667cce
Daniel Worthy <daniel@revunit.com>
2021-02-23 09:04:16 -0600

    Stuff

--- door.tsx
+++ door.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="door" tilewidth="32" tileheight="32" tilecount="192" columns="12">
- <image source="door.png" width="384" height="512"/>
+<tileset version="1.4" tiledversion="1.4.3" name="door" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="images/door.png" width="384" height="512"/>
 </tileset>
