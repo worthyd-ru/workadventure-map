f4459efff4fed3565e766ca02722542c57213524
Daniel Worthy <daniel@revunit.com>
2021-02-25 08:25:33 -0600

    udpates

--- old/fence.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="fence" tilewidth="32" tileheight="32" tilecount="90" columns="10">
- <image source="fence.png" width="320" height="288"/>
-</tileset>
