45fd4eec402476eb51a728cbc7363e9f374fb838
worthyd-ru <57643926+worthyd-ru@users.noreply.github.com>
2021-02-25 14:26:22 +0000

    Deploying to gh-pages from master @ f4459efff4fed3565e766ca02722542c57213524 🚀

---
+++ megasheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="megasheet" tilewidth="32" tileheight="32" tilecount="2016" columns="32">
+ <image source="images/megasheet.jpg" width="1024" height="2016"/>
+</tileset>
--- old/door.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="door" tilewidth="32" tileheight="32" tilecount="192" columns="12">
- <image source="door.png" width="384" height="512"/>
-</tileset>
