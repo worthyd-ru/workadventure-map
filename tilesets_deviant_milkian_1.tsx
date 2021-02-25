78c1958303e12e1357f5a69e35c1d6d273903f98
worthyd-ru <57643926+worthyd-ru@users.noreply.github.com>
2021-02-25 15:01:54 +0000

    Deploying to gh-pages from master @ 1fcc6d355dcaf0aa7961513c67f3d5cb27a9642d 🚀

--- tilesets_deviant_milkian_1.tsx
+++ tilesets_deviant_milkian_1.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.5" tiledversion="2021.02.15" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="images/tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <tile id="116">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
