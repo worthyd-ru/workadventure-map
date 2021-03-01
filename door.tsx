09a4aa33a05b1f03107812ca94806b035bf0165c
worthyd-ru <57643926+worthyd-ru@users.noreply.github.com>
2021-03-01 14:31:53 +0000

    Deploying to gh-pages from master @ d0ae75b5c9584f9b85635242be0ec974f5e627b4 🚀

--- door.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="door" tilewidth="32" tileheight="32" tilecount="192" columns="12">
- <image source="images/door.png" width="384" height="512"/>
-</tileset>
--- fence.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="fence" tilewidth="32" tileheight="32" tilecount="90" columns="10">
- <image source="images/fence.png" width="320" height="288"/>
-</tileset>
